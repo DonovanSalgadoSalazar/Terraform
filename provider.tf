171734b97d150fd34521384bfdf7bb182994a4cc
Donovan <doonooekinoxx@gmail.com>
2026-07-05 17:52:21 -0600

    D

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  resource_provider_registrations = "none"
+ 
   subscription_id                 = var.subscription_id
   tenant_id                       = var.tenant_id
 
